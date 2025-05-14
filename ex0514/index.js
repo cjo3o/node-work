7a7c3efc70912fa5f01dfcd7191e5238768e4583
cjo3o <cjy55973743@gmail.com>
2025-05-14 17:35:12 +0900

    no message

--- ex0514/index.js
+++ ex0514/index.js
@@ -2,14 +2,47 @@ const http = require('http');
 const fs = require('fs').promises;
 
 http.createServer(async (req, res) => {
+    console.log(req.url);
 
     try {
-        const password = "비밀번호";
-        const indexhtml = await fs.readFile('./index.html');
+        if (req.url === '/') {
+            const password = "비밀번호";
+            const indexhtml = await fs.readFile('./index.html');
 
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.end(indexhtml);
-    } catch (err) {
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            return res.end(indexhtml);
+        } else if (req.url === '/join' && req.method === 'POST') {
+
+            req.setEncoding('utf-8');
+
+            let body = '';
+            req.on('data', (data) => {
+                body += data;
+            });
+            req.on('end', () => {
+                console.log(body);
+
+                const {id, password} = JSON.parse(body);
+                console.log(id, password);
+            })
+
+            console.log(body);
+
+            const obj = {
+                name: '홍길동',
+                age: 20
+            }
+            res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'});
+            return res.end(JSON.stringify(obj));
+        } else if (req.url === '/login') {
+            res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+            return res.end('로그인 성공');
+        } else {
+            res.writeHead(404, ({'Content-Type': 'text/plain; charset=utf-8'}));
+            return res.end('잘못된 경로입니다.')
+        }
+    } catch
+        (err) {
         console.error(err);
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
         res.end(err.message);
