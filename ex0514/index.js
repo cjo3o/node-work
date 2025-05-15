f3d1b31c8c2b63faa0c985d3c37a9730afe88687
cjo3o <cjy55973743@gmail.com>
2025-05-15 12:13:59 +0900

    no message

--- ex0514/index.js
+++ ex0514/index.js
@@ -1,3 +1,6 @@
+const pool = require('./db');
+console.log(pool);
+
 const http = require('http');
 const fs = require('fs').promises;
 
@@ -11,6 +14,16 @@ http.createServer(async (req, res) => {
 
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
             return res.end(indexhtml);
+        } else if (req.url.includes('select')) {
+            const conn = await pool.getConnection();
+            const sql = 'SELECT * FROM users';
+            const [rows] = await conn.execute(sql);
+
+            conn.release();
+            console.log(rows);
+
+            res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+            return res.end(JSON.stringify(rows));
         } else if (req.url === '/join' && req.method === 'POST') {
 
             req.setEncoding('utf-8');
@@ -19,21 +32,25 @@ http.createServer(async (req, res) => {
             req.on('data', (data) => {
                 body += data;
             });
-            req.on('end', () => {
+            req.on('end', async () => {
                 console.log(body);
 
                 const {id, password} = JSON.parse(body);
                 console.log(id, password);
+
+                // mysql에 저장하는 코드
+                const conn = await pool.getConnection(); // pool에서 connection을 가져온다.
+                const sql = 'INSERT INTO users (id, password) VALUES (?,?)'; // sql 구문 설정
+                const [result] = await conn.execute(sql, [id, password]); // sql문 실행
+                conn.release(); // pool 반환
+
+                console.log(result); // 결과 출력해보기
             })
 
             console.log(body);
-
-            const obj = {
-                name: '홍길동',
-                age: 20
-            }
+            
             res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'});
-            return res.end(JSON.stringify(obj));
+            return res.end(JSON.stringify({message:'회원가입 성공'}));
         } else if (req.url === '/login') {
             res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
             return res.end('로그인 성공');
