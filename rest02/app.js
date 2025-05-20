e92fd3a3605a8938c4467915e8a41508e080f9fb
cjo3o <cjy55973743@gmail.com>
2025-05-20 13:20:58 +0900

    no message

--- rest02/app.js
+++ rest02/app.js
@@ -1,12 +1,32 @@
+const pool = require("./db");
 const express = require('express');
+const path = require("node:path");
 const app = express();
+const morgan = require("morgan");
 
-app.get('/', (req, res) => {
-    res.send('Hello Get');
+app.use(morgan("dev"));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+    console.log("모든 요청은 여기 들렸다가 진행");
+    next();
 })
 
-app.post('/', (req, res) => {
-    res.send('Hello Post');
+app.get('/', async (req, res) => {
+    const conn = await pool.getConnection();
+    const result = await conn.execute('select * from users');
+    console.log(result);
+    conn.release();
+    res.send(result[0]);
+})
+
+app.post('/', async (req, res) => {
+    const conn = await pool.getConnection();
+    const result = await conn.execute('insert into users (id, password) values (`${req.body.name}`, `${req.body.age}`)');
+    conn.release();
+    res.send(result);
 })
 
 app.put('/', (req, res) => {
@@ -15,6 +35,15 @@ app.put('/', (req, res) => {
 
 app.delete('/', (req, res) => {
     res.send('Hello Delete');
+})
+
+app.get('/html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('error');
 })
 
 app.listen(8080, () => {
