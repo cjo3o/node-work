d4639bbabca4416f7421c43aec0e9931496b3a2d
cjo3o <cjy55973743@gmail.com>
2025-06-12 10:07:55 +0900

    no message

---
+++ back05/routes/driverRouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const supabase = require("../config/supa.js");
+
+router = express.Router();
+
+router.route("")
+    .get(async (req, res, next) => {
+
+        const {data, error} = await supabase.from('ice_res').select();
+
+        res.render('driver', {data});
+    })
+    .post(async (req, res, next) => {
+        console.log(req.body);
+        return res.send("저장됨");
+    });
+
+module.exports = router;
---
+++ back05/routes/resRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const supabase = require("../config/supa.js");
+
+const router = express.Router();
+
+router.post("/save", async (req, res, next) => {
+    console.log(req.body);
+    console.log('api Post 통신');
+
+    const data = await supabase
+        .from('ice_res')
+        .insert([req.body])
+
+    if (data.status === 201) {
+        res.send("success");
+    } else {
+        res.send("fail");
+    }
+});
+
+module.exports = router;
